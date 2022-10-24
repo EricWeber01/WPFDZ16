16aaa41c300e10568ee764ad748f74ecce2c7bb1
babya <babya@ERICWEBER>
2022-10-24 13:13:06 +0300

    Add project files.

---
+++ WPFDZ16/UserWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace WPFDZ16
+{
+    /// <summary>
+    /// Interaction logic for UserWindow.xaml
+    /// </summary>
+    public partial class UserWindow : Window
+    {
+        public User User { get; private set; }
+        public UserWindow(User user)
+        {
+            InitializeComponent();
+            User = user;
+            DataContext = User;
+        }
+
+        void Accept_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
